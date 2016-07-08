4fb2dabcf56e92f505f1b237aefdf7b6ad76a418
Bruce Grover Lee <brucegrover.lee@gmail.com>
2016-07-07 22:47:21 -0500

    se cambio de font de google

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,600,100,300' rel='stylesheet' type='text/css'>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
